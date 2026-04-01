930941bc7522dcd4af7cfca85ba9c0e150fa3347
alex-huff <alexhuff@bakahuff.com>
2026-04-01 18:19:54 -0500

    fix: 'AutoConfigClient' to generate the config screen

--- src/main/java/com/alexfh/external_text_edit/config/ExternalTextEditModMenuImpl.java
+++ src/main/java/com/alexfh/external_text_edit/config/ExternalTextEditModMenuImpl.java
@@ -2,15 +2,13 @@ package com.alexfh.external_text_edit.config;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.clothconfig2.api.ConfigBuilder;
-import net.minecraft.network.chat.Component;
+import me.shedaniel.autoconfig.AutoConfigClient;
 
 public class ExternalTextEditModMenuImpl implements ModMenuApi
 {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory()
     {
-        return parent ->
-                ConfigBuilder.create().setParentScreen(parent).setTitle(Component.translatable("text.autoconfig.external-text-edit.title")).build();
+        return parent -> AutoConfigClient.getConfigScreen(ExternalTextEditConfig.class, parent).get();
     }
 }
